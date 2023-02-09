5c9963b9fb5b4614ef51690a60379b332b595d38
Snehal979 <bansodsnehal535@gmail.com>
2023-02-09 21:51:58 +0530

    [Snehal]Add: logical program demo

---
+++ LogicalProgramDemo/LogicalProgramDemo/PerfectNumber/PerfectNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramDemo.PerfectNumber
+{
+    public class Perfect
+    {
+        public void CheckPerfectNumber(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if(num %i ==0)
+                {
+                    sum = sum +i;
+                }
+            }
+            if(num == sum)
+            {
+                Console.WriteLine("{0} Perfect number",num);
+            }
+            else
+            {
+                Console.WriteLine("{0} Perfect number", num);
+            }
+        }
+        
+    }
+}
---
+++ LogicalProgramDemo/LogicalProgramDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using LogicalProgramDemo.PerfectNumber;
+using System.Runtime.InteropServices;
+
+namespace LogicalProgramDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+          //    Perfect perfect = new Perfect();
+          //    perfect.CheckPerfectNumber(28);
+
+            int num = 3453;
+            int remainder = 0, revese = 0;
+            while(num !=0)
+            {
+                remainder = num%10;
+                revese=(revese*10)+remainder;
+                num= num /10;
+            }
+            
+            Console.WriteLine(revese);
+        }
+    }
+}
